98a69540438094f9837c7c66cb95fd7c46975be2
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-10-02 19:42:55 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
+"""
+TeraBox Leech Bot - Telegram Bot for downloading TeraBox files
+"""
 
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__description__ = "Telegram bot for leeching TeraBox files"
